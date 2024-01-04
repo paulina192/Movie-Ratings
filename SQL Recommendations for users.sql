548fd9a34111eb082e305b9f494f92dae6caaaca
paulina192 <127040766+paulina192@users.noreply.github.com>
2024-01-04 17:16:08 +0100

    SQL Recommendations for users

--- SQL Recommendations for users.sql
+++ SQL Recommendations for users.sql
@@ -63,6 +63,37 @@ AND t3.movie_id NOT IN ( SELECT movies.movie_id
 GROUP BY t3.title, t3.director, t3.genre, t3.running_time, t3.language, t3.release_year
 HAVING AVG(t2.rating) > 3.5 
 
+--User 5:
+SELECT t3.title, t3.director, t3.genre, t3.running_time, t3.language, t3.release_year
+FROM user_ratings t1 INNER JOIN user_ratings t2
+ON t1.user_id = 5
+AND t2.user_id != 5
+INNER JOIN movies t3
+ON t2.movie_id = t3.movie_id
+AND t3.movie_id NOT IN ( SELECT movies.movie_id
+                         FROM movies
+			 INNER JOIN user_ratings
+			 ON movies.movie_id = user_ratings.movie_id
+                         WHERE user_id = 5)
+GROUP BY t3.title, t3.director, t3.genre, t3.running_time, t3.language, t3.release_year
+HAVING AVG(t2.rating) > 3.5 
+
+--User 6:
+SELECT t3.title, t3.director, t3.genre, t3.running_time, t3.language, t3.release_year
+FROM user_ratings t1 INNER JOIN user_ratings t2
+ON t1.user_id = 6
+AND t2.user_id != 6
+INNER JOIN movies t3
+ON t2.movie_id = t3.movie_id
+AND t3.movie_id NOT IN ( SELECT movies.movie_id
+                         FROM movies
+			 INNER JOIN user_ratings
+			 ON movies.movie_id = user_ratings.movie_id
+                         WHERE user_id = 6)
+GROUP BY t3.title, t3.director, t3.genre, t3.running_time, t3.language, t3.release_year
+HAVING AVG(t2.rating) > 3 
+ORDER BY running_time	
+
 --User 7:
 SELECT t3.title, t3.director, t3.genre, t3.running_time, t3.language, t3.release_year
 FROM user_ratings t1 INNER JOIN user_ratings t2
